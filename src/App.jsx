d296fc975be68c80aa6380465d6f6c5f6291d10f
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2023-06-01 17:47:06 -0300

    iniciando componente Header e estabelecendo estrutura de pastas

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
+import { Header } from "./components/header";
+
 export function App() {
 
   return (
-    <header>
-      <h1>HM Blog</h1>
-
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>Diário online</li>
-        </ul>
-      </nav>
-
-    </header>
+    <Header />
   )
 }
---
+++ src/components/header.jsx
@@ -0,0 +1,15 @@
+export function Header() {
+  return (
+    <header>
+      <h1>HM Blog</h1>
+
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Diário online</li>
+        </ul>
+      </nav>
+
+    </header>
+  )
+}
