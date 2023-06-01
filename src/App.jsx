7ca7ccc1b5bee8f4f3b077046b0933b63e3d1f69
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2023-06-01 18:15:49 -0300

    adicionando roteamento com react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import { Header } from "./components/header";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 export function App() {
 
   return (
     <Header />
+
+    <Router>
+      <Switch>
+        <Route path="/" exact Component={Home}></Route>
+        <Route path="/" Component={Diario}></Route>
+      </Switch>
+    </Router>
   )
 }
