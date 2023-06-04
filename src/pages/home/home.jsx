675c1b1097b6823ae32badc8b4e6de55829e780b
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2023-06-04 17:54:04 -0300

    page home finalizada

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,11 @@
+import './homestyle.css'
+
 export function Home () {
   return (
-    <div>oi</div>
+    <div className='containerHome'>
+      <div className='shadow'></div>
+      <h2 className='tittleHome'>React Blog</h2>
+      <p className='textHome'>Blog pessoal. Projeto feito para prática do conceito de Roteamento utilizando a biblioteca React Router DOM</p>
+    </div>
   )
 }
