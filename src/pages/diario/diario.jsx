a93f03e20e3c0c60755ebff674e20557781b096a
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2023-06-04 19:29:30 -0300

    alterações diversas

--- src/pages/diario/diario.jsx
+++ src/pages/diario/diario.jsx
@@ -3,12 +3,19 @@ import './diariostyle.css'
 export function Diario () {
   return (
     <div className='containerDiario'>
-      <button className="bookmarkBtn">
-        <span className="IconContainer"> 
-          <p className='icon'>&#x1F4DD;</p>
-        </span>
-        <p className="text">CRIAR</p>
-      </button>
+
+      <div className='divButton'>
+        <button className="bookmarkBtn">
+          <span className="IconContainer">
+            <p className='icon'>&#x1F4DD;</p>
+          </span>
+          <p className="text">CRIAR</p>
+        </button>
+      </div>
+
+      <div className='divConteudo'>
+        <p>teste</p>
+      </div>
 
     </div>
   )
