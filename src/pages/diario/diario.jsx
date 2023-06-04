ea7e1f60d849e13ca658a2afc67ac9929b7c6f7a
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2023-06-04 19:01:06 -0300

    criando botão

--- src/pages/diario/diario.jsx
+++ src/pages/diario/diario.jsx
@@ -1,5 +1,15 @@
+import './diariostyle.css'
+
 export function Diario () {
   return (
-    <div>oi2</div>
+    <div>
+      <button className="bookmarkBtn">
+        <span className="IconContainer"> 
+          <p className='icon'>&#x1F4DD;</p>
+        </span>
+        <p className="text">Criar</p>
+      </button>
+
+    </div>
   )
 }
