ff2710c6e2346c743b12aa577695da69796b9ea1
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2023-06-05 16:03:19 -0300

    iniciando painel de criação para novas anotações

---
+++ src/components/painelcriacao/painel.jsx
@@ -0,0 +1,14 @@
+import './painelstyle.css'
+
+export function Painel() {
+
+
+  return (
+    <div className='painelContainer'>
+
+      <input className='inputAnotacao' value='oi' type="text" />
+      <textarea className='textAnotacao' cols="30" rows="10">Olá</textarea>
+
+    </div>
+  )
+}
--- src/pages/diario/diario.jsx
+++ src/pages/diario/diario.jsx
@@ -1,15 +1,21 @@
 import { useState } from 'react'
 import { Anotacao } from '../../components/anotacao/anotacao'
 import './diariostyle.css'
+import { Painel } from '../../components/painelcriacao/painel'
 
 export function Diario () {
 
-  const [anotacoes, setAnotacoes] = useState([{tittle: 'oi', text: 'esse é meu textooooo'}, {tittle: 'opa', text: 'isto é um texto graaaanndeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'}, {tittle: 'olá', text: 'esse é meu texto pela segunda vez'}])
+  const [anotacoes, setAnotacoes] = useState([{tittle: 'opa', text: 'isto é um texto graaaanndeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'}])
+
+  const [classToggle, setClassToggle] = useState(false)
+
+
+
 
   return (
     <div className='containerDiario'>
 
-      <div className='divButton'>
+      <div className='divButton' onClick={() => setClassToggle(!classToggle)}>
         <button className="bookmarkBtn">
           <span className="IconContainer">
             <p className='icon'>&#x1F4DD;</p>
@@ -18,9 +24,13 @@ export function Diario () {
         </button>
       </div>
 
-      <div className='divConteudo'>
-        {anotacoes.map( item => <Anotacao tittle={item.tittle} text={item.text}/>)}
-      </div>
+      {!classToggle && <div className='divConteudo'>
+        {anotacoes.map( item => <Anotacao tittle={item.tittle} text={item.text} />)}
+      </div>}
+
+      
+
+      {classToggle && <div className="divConteudo"><Painel/></div>}
 
     </div>
   )
